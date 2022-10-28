1cc4407951df7c78067bd36c29ecdb655cb3c0fa
st4pzz <ramellajr@gmail.com>
2022-10-28 01:37:32 -0300

    commit

--- src/atoms/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as S from "./styles";
-
-const Button = () => {
-  return <S.Container>Login</S.Container>;
-};
-
-export default Button;
--- src/organisms/Header/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Box = styled.header`
-  padding-top: 30px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
